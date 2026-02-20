2db46802dbac77cbca6ae104fefe23ff8cea1324
Juan Manuel <juanmanuel@prueba.com>
2026-02-19 20:46:07 -0500

    Inicio proyecto 6

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    
+    let nota = document.getElementById("nota").value;
+
+    nota = parseFloat(nota)
+
+    console.log    
+
+    let resultado = document.getElementById("resultado");
+
+    
+    if(nota > 5 || nota < 0){
+        resultado.textContent = "Nota invalida"
+        resultado.style.color="yellow"
+    }else if(nota >= 3){
+        resultado.textContent = "Aprobo";
+        resultado.style.color="green"
+       }else{
+        resultado.textContent = "No aprobo"
+        resultado.style.color="red"      
+        }      
+})
